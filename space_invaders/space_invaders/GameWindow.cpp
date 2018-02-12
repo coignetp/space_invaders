3af78c406b497f006e054917143e3911d97e4521
Paul Coignet <crushing.of.files@gmail.com>
2018-02-12 14:36:22 +0100

    Add (GameWindow) layers

--- space_invaders/space_invaders/GameWindow.cpp
+++ space_invaders/space_invaders/GameWindow.cpp
@@ -7,8 +7,14 @@
 
 namespace graphics {
 	GameWindow::GameWindow(sf::VideoMode vid, const sf::String &title, sf::Uint32 style, const sf::ContextSettings &settings) :
-		sf::RenderWindow(vid, title, style, settings)
+		sf::RenderWindow(vid, title, style, settings),
+		m_layers(WINDOW_LAYER_TOTAL)
 	{
 
+	}
+
+	std::vector<sf::RenderTexture>& GameWindow::getRealLayers()
+	{
+		return m_layers;
 	}
 }
--- space_invaders/space_invaders/GameWindow.h
+++ space_invaders/space_invaders/GameWindow.h
@@ -5,11 +5,23 @@
 
 namespace graphics {
 
+	enum WINDOW_LAYERS { WINDOW_LAYER_BG, WINDOW_LAYER_ITEM, WINDOW_LAYER_TEXT, WINDOW_LAYER_TOTAL};
+
 	class GameWindow :
 		public sf::RenderWindow
 	{
 	public:
 		GameWindow(sf::VideoMode vid, const sf::String &title, sf::Uint32 style = sf::Style::Default, const sf::ContextSettings &settings = sf::ContextSettings());
+	
+
+		/**
+		* \brief Update the render texture if necessary	.
+		*
+		* \return the real std::vector<sf::RenderTexture>.
+		*/
+		std::vector<sf::RenderTexture>& getRealLayers();
+	private:
+		std::vector<sf::RenderTexture> m_layers;
 	};
 
 }
