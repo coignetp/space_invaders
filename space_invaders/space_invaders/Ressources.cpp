2b7c5c972f6708458588e2f2ba1418d44166d5ee
Paul Coignet <crushing.of.files@gmail.com>
2018-02-13 13:30:47 +0100

    Add (Ressources) load implementation

--- space_invaders/space_invaders/Ressources.cpp
+++ space_invaders/space_invaders/Ressources.cpp
@@ -19,6 +19,20 @@ namespace graphics {
 
 	int Ressources::load()
 	{
+		sf::Texture textu;
+
+		if (!textu.loadFromFile("data/bg.png"))
+			return -1;
+		m_textures.insert(std::make_pair(1, textu));
+
+		if (!textu.loadFromFile("data/me.png"))
+			return -1;
+		m_textures.insert(std::make_pair(2, textu));
+
+		if (!textu.loadFromFile("data/enemi.jpg"))
+			return -1;
+		m_textures.insert(std::make_pair(3, textu));
+
 		return 0;
 	}
 }
