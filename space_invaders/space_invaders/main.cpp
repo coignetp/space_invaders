5476992de260580b46489c0dab3804e42dbe15c1
Paul Coignet <crushing.of.files@gmail.com>
2018-02-16 13:03:35 +0100

    Update (main.cpp)

--- space_invaders/space_invaders/main.cpp
+++ space_invaders/space_invaders/main.cpp
@@ -3,23 +3,7 @@
 
 int main()
 {
-	Game g(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	Game g(sf::VideoMode(1024, 800), "Space Invaders");
 
-	g.getRealWindow().getRealLayers().at(0).draw(shape);
-
-	while (g.getRealWindow().isOpen())
-	{
-		sf::Event event;
-		while (g.getRealWindow().pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				g.getRealWindow().close();
-		}
-
-		g.getRealWindow().print();
-	}
-
-	return 0;
+	return g.start();
 }
