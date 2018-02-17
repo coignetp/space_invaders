8bb7b36cb01748079c327eae1e4799bef13c529d
Paul Coignet <crushing.of.files@gmail.com>
2018-02-17 12:05:11 +0100

    Add (Ship) position attribut

--- space_invaders/space_invaders/Ship.cpp
+++ space_invaders/space_invaders/Ship.cpp
@@ -10,3 +10,17 @@ Ship::Ship()
 Ship::~Ship()
 {
 }
+
+
+sf::Vector2i Ship::getPosition() const
+{
+	return m_position;
+}
+
+
+sf::Vector2i Ship::setPosition(const sf::Vector2i &pos)
+{
+	m_position = pos;
+
+	return m_position;
+}
--- space_invaders/space_invaders/Ship.h
+++ space_invaders/space_invaders/Ship.h
@@ -1,10 +1,29 @@
 #pragma once
 
+#include <SFML\Graphics.hpp>
 
 class Ship
 {
 public:
 	Ship();
 	~Ship();
+public:
+	/**
+	* \brief Gives the ship position. Const method.
+	*
+	* \return sf::Vector2i position.
+	*/
+	sf::Vector2i getPosition() const;
+	/**
+	* \brief Updates the ship position. Can be outside the screen.
+	*
+	* \param pos : New ship position.
+	*
+	* \return sf::Vector2i new position.
+	*/
+	sf::Vector2i setPosition(const sf::Vector2i &pos);
+
+private:
+	sf::Vector2i m_position;
 };
 
