b5f60efbcba0444d3f5ee37743f10b9da19b754d
Paul Coignet <crushing.of.files@gmail.com>
2018-02-17 12:14:52 +0100

    Add (Ship) move method

--- space_invaders/space_invaders/Ship.cpp
+++ space_invaders/space_invaders/Ship.cpp
@@ -24,3 +24,12 @@ sf::Vector2i Ship::setPosition(const sf::Vector2i &pos)
 
 	return m_position;
 }
+
+
+sf::Vector2i Ship::move(const int &dx, const int &dy)
+{
+	m_position.x += dx;
+	m_position.y += dy;
+
+	return m_position;
+}
--- space_invaders/space_invaders/Ship.h
+++ space_invaders/space_invaders/Ship.h
@@ -22,6 +22,15 @@ public:
 	* \return sf::Vector2i new position.
 	*/
 	sf::Vector2i setPosition(const sf::Vector2i &pos);
+	/**
+	* \brief Updates the ship position.
+	*
+	* \param dx : Number of pixel crossed (horizontal). Can be negative.
+	* \param dy : Number of pixel crossed (vertical). Can be negative.
+	*
+	* \return sf::Vector2i new position.
+	*/
+	sf::Vector2i move(const int &dx, const int &dy);
 
 private:
 	sf::Vector2i m_position;
