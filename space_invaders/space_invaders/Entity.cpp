f5e3ee4fba24487e5154097151b2e8c4e5df13b7
Paul Coignet <crushing.of.files@gmail.com>
2018-02-19 14:04:50 +0100

    Add (Entity) paste implementation of Ship

--- space_invaders/space_invaders/Entity.cpp
+++ space_invaders/space_invaders/Entity.cpp
@@ -1,12 +1,108 @@
 #include "Entity.h"
+#include "GameWindow.h"
 
 
-
-Entity::Entity()
+Entity::Entity() :
+	m_position(0, 0),
+	m_lastUpdate()
 {
 }
 
 
 Entity::~Entity()
 {
+}
+
+
+sf::Vector2i Entity::getPosition() const
+{
+	return m_position;
+}
+
+
+sf::Vector2i Entity::setPosition(const sf::Vector2i &pos)
+{
+	m_position = pos;
+
+	m_hitbox.getRealRect().left = pos.x;
+	m_hitbox.getRealRect().top = pos.y;
+
+	m_sprite->setPosition(sf::Vector2f(pos));
+
+	return m_position;
+}
+
+
+sf::Vector2f Entity::getSpeed() const
+{
+	return m_speed;
+}
+
+
+sf::Vector2f Entity::setSpeed(const sf::Vector2f &speed)
+{
+	m_speed = speed;
+
+	return m_speed;
+}
+
+
+sf::Vector2i Entity::move(const int &dx, const int &dy, const sf::Vector2i &wall)
+{
+	if ((wall.x == -1 && wall.y == -1)
+		|| (m_hitbox.getRealRect().left + dx >= 0
+			&& m_hitbox.getRealRect().left + m_hitbox.getRealRect().width + dx <= wall.x
+			&& m_hitbox.getRealRect().top + dy >= 0
+			&& m_hitbox.getRealRect().top + m_hitbox.getRealRect().height + dy <= wall.y))
+	{
+		m_position.x += dx;
+		m_position.y += dy;
+
+		m_hitbox.getRealRect().left += dx;
+		m_hitbox.getRealRect().top += dy;
+
+		getRealSprite()->setPosition(sf::Vector2f(getPosition()));
+	}
+
+	return m_position;
+}
+
+
+physics::Hitbox Entity::getHitbox() const
+{
+	return m_hitbox;
+}
+
+
+physics::Hitbox &Entity::getRealHitbox()
+{
+	return m_hitbox;
+}
+
+
+std::shared_ptr<sf::Sprite> &Entity::getRealSprite()
+{
+	return m_sprite;
+}
+
+
+int Entity::print(graphics::GameWindow &win)
+{
+	getRealSprite()->setPosition(sf::Vector2f(getPosition()));
+	win.getRealLayers()[graphics::WINDOW_LAYER_ITEM]->addSprite(m_hitbox.getId());
+
+	return 0;
+}
+
+
+void Entity::update(const sf::Time &t, const sf::Vector2i &wall)
+{
+	if (m_speed.x != 0 || m_speed.y != 0)
+		m_lastUpdate += t;
+	if (m_lastUpdate.asSeconds()*m_speed.x >= 1 || m_lastUpdate.asSeconds()*m_speed.y >= 1
+		|| m_lastUpdate.asSeconds()*m_speed.x <= -1 || m_lastUpdate.asSeconds()*m_speed.y <= -1)
+	{
+		move((int)(m_lastUpdate.asSeconds()*m_speed.x), (int)(m_lastUpdate.asSeconds()*m_speed.y), wall);
+		m_lastUpdate = sf::Time();
+	}
 }
--- space_invaders/space_invaders/Entity.h
+++ space_invaders/space_invaders/Entity.h
@@ -1,8 +1,97 @@
 #pragma once
+
+#include <memory>
+#include <SFML\Graphics.hpp>
+#include "Hitbox.h"
+
+namespace graphics { class GameWindow; }
+
 class Entity
 {
 public:
 	Entity();
 	~Entity();
+
+public:
+	/**
+	* \brief Gives the entity position. Const method.
+	*
+	* \return sf::Vector2i position.
+	*/
+	sf::Vector2i getPosition() const;
+	/**
+	* \brief Updates the entity position. Can be outside the screen.
+	*
+	* \param pos : New entity position.
+	*
+	* \return sf::Vector2i new position.
+	*/
+	sf::Vector2i setPosition(const sf::Vector2i &pos);
+	/**
+	* \brief Gives the entity speed. Const method.
+	*
+	* \return sf::Vector2f speed.
+	*/
+	sf::Vector2f getSpeed() const;
+	/**
+	* \brief Updates the entity speed. Can be outside the screen.
+	*
+	* \param speed : New entity speed.
+	*
+	* \return sf::Vector2f new position.
+	*/
+	sf::Vector2f setSpeed(const sf::Vector2f &speed);
+	/**
+	* \brief Updates the entity position.
+	*
+	* \param dx : Number of pixel crossed (horizontal). Can be negative.
+	* \param dy : Number of pixel crossed (vertical). Can be negative.
+	*
+	* \return sf::Vector2i new position.
+	*/
+	sf::Vector2i move(const int &dx, const int &dy, const sf::Vector2i &wall);
+
+	/**
+	* \brief Gives the entity hitbox. Const method.
+	*
+	* \return physics::Hitbox entity hitbox.
+	*/
+	physics::Hitbox getHitbox() const;
+	/**
+	* \brief Gives a real reference to the hitbox.
+	*
+	* \return physics::Hitbox& entity hitbox.
+	*/
+	physics::Hitbox &getRealHitbox();
+
+	/**
+	* \brief Gives a real reference to the sprite shared pointer.
+	*
+	* \return std::shared_ptr<sf::Sprite>& entity sprite pointer.
+	*/
+	std::shared_ptr<sf::Sprite> &getRealSprite();
+
+	/**
+	* \brief Prints the entity on the screen
+	*
+	* \return int error code
+	*/
+	int print(graphics::GameWindow &win);
+	/**
+	* \brief Updates the entity position according to the speed.
+	*
+	* \param t : time since the last update call
+	* \param wall : screen border
+	*
+	* \return nothing
+	*/
+	void update(const sf::Time &t, const sf::Vector2i &wall);
+
+private:
+	sf::Vector2i m_position;
+	sf::Vector2f m_speed;
+	physics::Hitbox m_hitbox;
+	std::shared_ptr<sf::Sprite> m_sprite;
+	sf::Time m_lastUpdate;
 };
 
