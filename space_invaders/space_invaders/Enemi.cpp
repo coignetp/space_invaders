8475bf849298cabf4805875f7051989bf1a22ee0
Paul Coignet <crushing.of.files@gmail.com>
2018-02-22 13:17:44 +0100

    Add (Enemi) walls attribut

--- space_invaders/space_invaders/Enemi.cpp
+++ space_invaders/space_invaders/Enemi.cpp
@@ -2,11 +2,26 @@
 
 
 
-Enemi::Enemi()
+Enemi::Enemi(const sf::Rect<int> &walls) :
+	m_walls(walls)
 {
 }
 
 
 Enemi::~Enemi()
 {
+}
+
+
+sf::Rect<int> Enemi::getWalls() const
+{
+	return m_walls;
+}
+
+
+sf::Rect<int> Enemi::setWalls(const sf::Rect<int> &walls)
+{
+	m_walls = walls;
+
+	return walls;
 }
--- space_invaders/space_invaders/Enemi.h
+++ space_invaders/space_invaders/Enemi.h
@@ -6,7 +6,25 @@ class Enemi :
 	public Ship
 {
 public:
-	Enemi();
+	Enemi(const sf::Rect<int> &walls);
 	~Enemi();
+
+public:
+	/**
+	* \brief Gives the walls of this enemi
+	*
+	* \return sf::Rect<int> walls
+	*/
+	sf::Rect<int> getWalls() const;
+	/**
+	* \brief Changes the walls
+	*
+	* \param wall : new walls
+	*
+	* \return sf::Rect<int> new walls
+	*/
+	sf::Rect<int> setWalls(const sf::Rect<int> &walls);
+private:
+	sf::Rect<int> m_walls;
 };
 
